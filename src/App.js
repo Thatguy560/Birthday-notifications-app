561c201d438a8f0fe7995b12a809843459144a96
Peter Stevens <peterstevens510@gmail.com>
2021-02-08 21:41:05 +0000

    birthday reminders being displayed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,44 @@
+import { useState } from "react";
+import { data } from "./birthday-data";
+
 function App() {
+  const [people, setPeople] = useState(data);
+
+  const removePerson = (id) => {
+    let newPerson = people.filter((person) => person.id !== id);
+    setPeople(newPerson);
+  };
+
   return (
-    <div>
-      <h1>Hello world</h1>
-    </div>
+    <>
+      <h2>
+        {people.length > 0
+          ? `${people.length} birthdays today`
+          : "All caught up on birthday reminders"}
+      </h2>
+      {people.map((person) => {
+        const { id, name, image } = person;
+        return (
+          <div className="container" key={id}>
+            <div className="avatar">
+              <div>
+                <img src={image} alt={name} />
+              </div>
+
+              <div>
+                <h3>{name}</h3>
+              </div>
+            </div>
+
+            <div className="button">
+              <button className="btn" onClick={() => removePerson(id)}>
+                Dismiss
+              </button>
+            </div>
+          </div>
+        );
+      })}
+    </>
   );
 }
 
