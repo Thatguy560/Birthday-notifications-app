0568a9081efc4b3c365824b93ff14c396a52c1a5
Peter Stevens <peterstevens510@gmail.com>
2021-03-02 16:27:44 +0000

    trying to incorporate facebook birthdays with birthdays app however doesn't seem possible, will possibly go back to later if it's been updated

--- src/facebookLogin.js
+++ src/facebookLogin.js
@@ -1,40 +1,52 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 import { render } from "react-dom";
 import FacebookLogin from "react-facebook-login";
-// import React, { Component } from "react";
+import { data } from "./birthday-data";
 
-const componentClicked = (data) => {
-  console.log("data", data);
-};
+class ReactFacebookLogin extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      accessToken: "",
+      name: "",
+    };
+  }
 
-const responseFacebook = (response) => {
-  console.log(response);
-};
+  componentClicked = (data) => {
+    console.log("data", data);
+  };
 
-const ReactFacebookLogin = () => {
-  return (
-    <div>
-      {/* React Facebook Login */}
-      <FacebookLogin
-        appId="1088597931155576"
-        autoLoad={true}
-        fields="name,email,picture"
-        onClick={componentClicked}
-        callback={responseFacebook}
-      />
-    </div>
-  );
-};
+  updateName = () => {
+    this.setState({ name: "Peter" });
+  };
+
+  responseFacebook = (response) => {
+    console.log(response);
+    console.log(this.state);
+    // console.log(response.friends.data);
+    // console.log(response.picture.data.url);
+    this.setState({ accessToken: response.accessToken, name: "Peter" });
+  };
+
+  render() {
+    return (
+      <div>
+        {/* User Short-Life Access Token: */}
+        <h1>{this.updateName}</h1>
+        <FacebookLogin
+          appId="952295725304264"
+          autoLoad={true}
+          fields="id,name,email,picture,birthday,friends"
+          scope="user_friends"
+          onClick={this.componentClicked}
+          callback={this.responseFacebook}
+        />
+        {this.state.accessToken}
+      </div>
+    );
+  }
+}
 
 export default ReactFacebookLogin;
 
-// 1) Ensure that months are ordered in the following e.g. Jan, feb march...
-// 2) Get sort by age function working
-// 3) // Try and see if you can implement facebook data (API) into birthdays
-
-// data.sort((a, b) => {
-//   return new Date(a.age).getFullYear() - new Date(b.age).getFullYear();
-// });
-
-// updateData(newPerson);
-// this.setState(newPerson: this.state.newPerson)
+// https://graph.facebook.com/4177740805589440/accounts?access_token=EAANiG6BticgBAPWzzHI4jQmUR5kZBGYl19rWuXp4n67swQQUIlCSE9NkucJoV6CJoPZBoCmdV63YnieZBOKpqhBBUxqDG2WnkA6wFBv7ZC1z7HmTtyEmNecwwkqRQm2kaTfQjEepJnQiIFfZChSzqHnwr1YEIIxBzAu6IQa2DzQZDZD
